c578b34f05011e2c02547b64e26c8e8c5a9b855d
Damir Vazgird <damir@withpulp.com>
2017-02-07 17:11:09 -0500

    updated json path

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,7 +1,7 @@
 webApp.controller('SettingsController', ['$scope', '$http', function($scope, $http){
   $scope.colorIcons = 'off';
 
-  $http.get('../assets/json/accounts.json')
+  $http.get('/assets/json/accounts.json')
     .then(function(res){
       $scope.accounts = res.data;
     });
