21495592544d0bf25da8f66cb358657dee6cf1ca
Damir Vazgird <damir@withpulp.com>
2017-02-06 22:09:57 -0500

    added most of the markup

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,3 +1,6 @@
-webApp.controller('SettingsController', ['$scope', function($scope){
-
+webApp.controller('SettingsController', ['$scope', '$http', function($scope, $http){
+  $http.get('../assets/json/accounts.json')
+       .then(function(res){
+          $scope.accounts = res.data;
+        });
 }]);
