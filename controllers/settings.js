47de01a640eb19b0b6f1e7eca47be50694c165ae
Damir Vazgird <damir@withpulp.com>
2017-02-07 02:34:42 -0500

    final touches

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,6 +1,8 @@
 webApp.controller('SettingsController', ['$scope', '$http', function($scope, $http){
+  $scope.colorIcons = 'off';
+
   $http.get('../assets/json/accounts.json')
-       .then(function(res){
-          $scope.accounts = res.data;
-        });
+    .then(function(res){
+      $scope.accounts = res.data;
+    });
 }]);
