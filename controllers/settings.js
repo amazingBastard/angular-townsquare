2fd240d20026f730d87b3d07f402a6e93e6fbdeb
Damir Vazgird <damir@withpulp.com>
2017-02-08 18:02:30 -0500

    icon colors toggle from switch

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,5 +1,6 @@
 webApp.controller('SettingsController', ['$scope', '$http', function($scope, $http){
   $scope.colorIcons = 'off';
+  $scope.color = false;
 
   $http.get('assets/json/accounts.json')
     .then(function(res){
